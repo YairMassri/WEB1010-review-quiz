908d489095579cacd951e9c00ccf2da6fb45bc3d
YairMassri <yair.masri@gmail.com>
2018-04-16 18:58:44 -0400

    First Commit

---
+++ Q3.js
@@ -0,0 +1,9 @@
+// Question 3
+
+// Inside of console.log, write a Boolean expression that tests
+// whether two numbers are NOT equal, and whose answer evaluates to true.
+
+var a = 4;
+var b = 6;
+
+console.log(a != b)
---
+++ Q4.js
@@ -0,0 +1,13 @@
+// Question 4
+
+// In a console.log, build the following string in one line of code
+// using special characters for the newline (\n), tab stop (\t), and
+// quotation marks (\"):
+//
+//      "It ain't over til
+//      it's over."
+//             - Yogi Berra
+
+var a = "\"It ain't over til\n\tit's over.\"\n\t- Yogi Berra"
+
+console.log(a)
